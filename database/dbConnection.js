356fb751b0b6174ed5eb42e51d7ee0cc2dbad0cc
Bhavishya Sahay <145124737+BhavishyaSahay@users.noreply.github.com>
2024-10-30 15:50:47 +0530

    Updated dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 export const dbConnection = async () => {
   const uri = process.env.MONGO_URI;
-  mongoose
-    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+  return mongoose
+    .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000,  // Set connection timeout to 10 seconds
+    })
     .then(() => console.log("Database connected"))
     .catch((err) => console.error("Database connection error:", err));
-
-  console.log(`MongoDB Connected: ${uri}`);
 };
